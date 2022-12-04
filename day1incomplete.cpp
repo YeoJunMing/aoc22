d914a431d1098905a985312855aa536acfab5e76
YeoJunMing <119714209+YeoJunMing@users.noreply.github.com>
2022-12-05 02:29:26 +0800

    Update day1incomplete.cpp

--- day1incomplete.cpp
+++ day1incomplete.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-
 
 int main() {
-  //stringing elf calories
-  std::string elfc;
+
+  std::string elfc; //string each value in the file
+ 
+  int calories = 0;
+  int maximum = 0; //will be the highest calories
   
   //reading from file
-  std::ifstream myfile ("data.txt");
+  std::ifstream myfile ("1.txt");
 
-  //make vector to have the total calories of each elf
-  std::vector<int>calories;
-  
-  //reading each line
   while (getline (myfile, elfc)) {
-    
-    //when it reads until an empty line
     if (elfc.empty()) {
-      //stoi(elfc) then add them up and the sum becomes an integer   
-      //this happens until the strings are read then output the largest integer in the vector
+          if (calories > maximum) { //record the highest calories
+      maximum = calories;
     }
-  std::cout << //max.element() idk how to code it
+      calories = 0; //ignore the calories of elf who are low
+      }
+   else {
+      calories = calories + std::stoi(elfc); //adds up the calories of one elf
     }
-
+  
+    }
+  std::cout << maximum;
 }
